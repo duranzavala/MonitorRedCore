605d9a2a0f4a43af244d47ad2e86c38b6cffcb37
Arnulfo <arnulfo.duran@wrethink.com>
2020-06-18 20:12:42 -0700

    Feat: Project created, also the infraestructure was define

---
+++ MonitorRedCore.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MonitorRedCore.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ MonitorRedCore.Infraestructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MonitorRedCore.Infraestructure
+{
+    public class Class1
+    {
+    }
+}
